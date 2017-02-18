5d2284a52eae8fe5a9c29e445bc89716e1f82bfe
makepost <makepost@firemail.cc>
2017-02-18 02:19:46 +0200

    App: Fix smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,16 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
+import Store from './store'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  const StoreInstance = Store()
+  ReactDOM.render(
+    <Provider store={StoreInstance}>
+      <App />
+    </Provider>,
+    document.createElement('div')
+  )
 })
