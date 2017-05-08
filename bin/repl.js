f7a8211f0008987a956aee801d5c53e086d9d720
makepost <makepost@firemail.cc>
2017-05-09 02:05:02 +0300

    Gjs repl with require for quick module checks

---
+++ bin/repl.js
@@ -0,0 +1,12 @@
+#!/usr/bin/gjs
+/* global imports */
+// Sets up the environment and runs a Gjs shell.
+
+const path = /^.*?@(.*):/.exec(new Error().stack)[1]
+const dirname = imports.gi.Gio.File.new_for_path(path).get_parent().get_parent().get_path()
+imports.searchPath.push(dirname)
+imports.src.utils.require.require()
+
+imports.gi.Gtk.init(null)
+require('../src/utils/GtkDom').require()
+imports.console.interact()
