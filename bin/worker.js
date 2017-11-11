11c686044cc2e0ecbbfbf5f79d2b0408b71e745b
makepost <makepost@firemail.cc>
2017-11-11 21:46:15 +0200

    Worker: Fix process.env missing

--- bin/worker.js
+++ bin/worker.js
@@ -5,6 +5,7 @@ const path = /^.*?@(.*):/.exec(new Error().stack)[1];
 const dirname = imports.gi.Gio.File.new_for_path(path).get_parent().get_parent().get_path();
 imports.searchPath.push(dirname);
 new imports.src.app.Gjs.Require.Require().require();
+new imports.src.app.Gjs.GtkDom.GtkDom().require();
 
 const { Worker } = require("../src/app/Gio/Worker");
 const worker = new Worker(JSON.parse(ARGV[0]), (action) => {
