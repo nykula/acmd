51a6423bf20950384f352df3b3c1a3b09595a9d3
makepost <makepost@firemail.cc>
2017-12-15 19:11:32 +0200

    Pulse: List

--- App.js
+++ App.js
@@ -22,59 +22,109 @@ function App(baseUrl) {
   this.repo = gh.getRepo(user, repo);
 }
 
+/**
+ * @static
+ * @param {HTMLElement} a
+ * @param {HTMLElement} b
+ */
+App.compare = function(a, b) {
+  const aKey = a.getAttribute("data-key") || "";
+  const bKey = b.getAttribute("data-key") || "";
+
+  return new Date(aKey).getTime() - new Date(bKey).getTime();
+};
+
 /** @type {(selector: string) => HTMLElement} */
 App.prototype.$ = document.querySelector.bind(document);
 
 /** @type {typeof console.log} */
 App.prototype.debug = console.log.bind(console);
 
-App.prototype.listIssues = function() {
-  this.issues.listIssues({ sort: "updated" })
-    .then((/** @type {any} */ res) => this.debug(res.data))
-    .catch(console.error);
-};
+App.prototype.listPulse = function() {
+  /**
+   * @typedef Resource
+   * @property {{ committer: { date: string }, message: string }} commit
+   * @property {string} html_url
+   *
+   * @typedef Issue
+   * @property {string} html_url
+   * @property {{ name: string }[]} labels
+   * @property {string} title
+   * @property {string} updated_at
+   */
 
-App.prototype.listCommits = function() {
+  /** @type {Resource[]} */
+  let resources;
+
+  /** @type {Issue[]} */
+  let issues;
+
   this.repo.listCommits()
     .then((/** @type {any} */ res) => {
-      /**
-       * @typedef User
-       * @property {string} html_url
-       * @property {string} name
-       *
-       * @typedef Commit
-       * @property {User} author
-       * @property {{ author: { date: string } }} commit
-       * @property {User} committer
-       * @property {string} html_url
-       */
-
-      /** @type {Commit[]} */
-      const commits = res.data;
-
-      this.debug(commits.map(commit => {
-        return {
-          author: {
-            html_url: commit.author.html_url,
-            name: commit.author.name,
-          },
-
-          commit: {
-            author: {
-              date: commit.commit.author.date,
-            },
-          },
-
-          committer: {
-            html_url: commit.committer.html_url,
-            name: commit.committer.name,
-          },
-
-          html_url: commit.html_url,
-        };
-      }));
+      resources = res.data.slice(0, 15);
+      return this.issues.listIssues({ sort: "updated" });
     })
-    .catch(console.error);
+    .then((/** @type {any} */ res) => {
+      issues = res.data;
+
+      const section = this.$(".Pulse");
+      const list = e("ul");
+      list.className = "list-unstyled";
+
+      for (let i = 0; i < issues.length; i++) {
+        const issue = issues[i];
+        const item = e("li");
+        item.setAttribute("data-key", issue.updated_at);
+
+        const title = e("p");
+        const link = e("a");
+        link.href = issue.html_url;
+        link.textContent = issue.title;
+        title.appendChild(link);
+
+        for (let j = 0; j < issue.labels.length; j++) {
+          const label = issue.labels[j];
+
+          const badge = e("span");
+          badge.className = "badge badge-secondary";
+          badge.textContent = label.name;
+
+          title.appendChild(document.createTextNode(" "));
+          title.appendChild(badge);
+        }
+
+        item.appendChild(title);
+        list.appendChild(item);
+      }
+
+      for (let i = 0; i < resources.length; i++) {
+        const resource = resources[i];
+
+        if (/^v\d+\.\d+\.\d+$/.test(resource.commit.message)) {
+          continue;
+        }
+
+        const item = e("li");
+        item.setAttribute("data-key", resource.commit.committer.date);
+
+        const title = e("p");
+        const link = e("a");
+        link.href = resource.html_url;
+        link.textContent = resource.commit.message.split("\n")[0];
+        title.appendChild(link);
+        item.appendChild(title);
+
+        list.appendChild(item);
+      }
+
+      Array.prototype.slice.call(list.children)
+        .sort(App.compare)
+        .reverse()
+        .forEach(list.appendChild.bind(list));
+
+      section.appendChild(list);
+    })
+    .catch(this.debug);
 };
 
 App.prototype.listReleases = function() {
@@ -137,7 +187,7 @@ App.prototype.listReleases = function() {
         prevMinor = version[2];
       }
     })
-    .catch(console.error);
+    .catch(this.debug);
 };
 
 /**
@@ -145,8 +195,7 @@ App.prototype.listReleases = function() {
  */
 App.prototype.render = function(parent) {
   this.$ = parent.querySelector.bind(parent);
-  this.listCommits();
-  this.listIssues();
+  this.listPulse();
   this.listReleases();
 };
 
