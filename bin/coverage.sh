2381a6fe6e2790ada31620c0121fdcc87a6d9524
makepost <makepost@firemail.cc>
2017-12-22 01:01:07 +0200

    Coverage: Native imports
    Since a5ba324, unless env is "development", CommonJS modules are loaded as closures. Gjs imports have coverage info and file paths in warnings but cause slow app start.

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -6,7 +6,7 @@ dir="$(realpath "$(dirname "$0")/..")"
 rm -rf "$dir"/coverage
 
 # Gjs coverage uses absolute paths.
-NODE_ENV=production gjs \
+NODE_ENV=development gjs \
   --coverage-prefix="$dir"/src \
   --coverage-output="$dir"/coverage \
   "$dir"/bin/test.js
